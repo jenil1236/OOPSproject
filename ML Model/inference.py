aabc9a1c6bdb6693f7d2444d28a8412cf68dd52e
Ayushman <singhayushman291@gmail.com>
2025-09-29 14:29:21 +0530

    treatment files added

--- ML Model/inference.py
+++ ML Model/inference.py
@@ -7,7 +7,7 @@ import json
 
 # Paths
 model_path = "D:/OOPS Project/ayushman/ML Model/pth_files/mango_classifier.pth"
-test_img_path = "D:/OOPS Project/ayushman/ML Model/test_inferences/infer3.jpg"
+test_img_path = "D:/OOPS Project/ayushman/ML Model/test_inferences/infer1.jpg"
 
 # Device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -19,15 +19,21 @@ transform = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
+# load class names
 with open("json_files/mango_classnames.json") as f:
     class_names = json.load(f)
 
+# Load treatments
+with open("json_files/mango_treatments.json") as f:
+    treatments = json.load(f)
+
 # Load model
-model = models.resnet18(pretrained=False)  # no pretrained, structure only
+model = models.resnet18(weights=None)  # no pretrained, structure only
 num_classes = len(class_names)
 model.fc = nn.Linear(model.fc.in_features, num_classes)
 
-model.load_state_dict(torch.load(model_path, map_location=device))
+state_dict = torch.load(model_path, map_location=device, weights_only=True)
+model.load_state_dict(state_dict)
 model.to(device)
 model.eval()
 
@@ -43,3 +49,11 @@ def predict_image(img_path):
 # Run prediction
 predicted_class = predict_image(test_img_path)
 print(f"Predicted class: {predicted_class}")
+
+# Show treatments
+if predicted_class in treatments:
+    print("Recommended treatments/chemicals:")
+    for t in treatments[predicted_class]:
+        print(f"- {t}")
+else:
+    print("No treatment info available for this class.")
